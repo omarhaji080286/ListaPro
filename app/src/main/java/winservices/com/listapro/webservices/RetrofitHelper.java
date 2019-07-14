657499cb33db53dcfc4ff4dc98ebd22f0a191326
omar.haji <omar.haji@gmail.com>
2019-07-14 17:51:14 +0100

    release 1.0.4

--- app/src/main/java/winservices/com/listapro/webservices/RetrofitHelper.java
+++ app/src/main/java/winservices/com/listapro/webservices/RetrofitHelper.java
@@ -6,13 +6,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitHelper {
 
     //Lista LOCAL
-    //private final static String HOST = "http://192.168.43.211/lista_local/webservices/";
+    private final static String HOST = "http://192.168.43.211/lista_local/webservices/";
 
     //Lista LWS_PRE_PROD
     //private final static String HOST = "http://lista-courses.com/lista_pre_prod/webservices/";
 
     //Lista LWS_PROD
-    private final static String HOST = "http://lista-courses.com/lista_prod/webservices/";
+    //private final static String HOST = "http://lista-courses.com/lista_prod/webservices/";
 
     public ListaProWebServices initWebServices() {
         Retrofit retrofit = new Retrofit.Builder()
